0dfe104b1490ff48cc3376b4c0adf95f36efd227
Simone Grignola <sito@grignola.ch>
2020-08-31 00:21:39 +0200

    Fixed USE_FULLEMAIL_FOR_LOGIN

--- config.php
+++ config.php
@@ -30,7 +30,7 @@ set_include_path(get_include_path() . PATH_SEPARATOR . $awlpath);
      *   false - use the username only.
      *   true  - string the mobile sends as username, e.g. full email address (default).
      */
-    define('USE_FULLEMAIL_FOR_LOGIN', false);
+    define('USE_FULLEMAIL_FOR_LOGIN', true);
 
 /**********************************************************************************
  * StateMachine setting
